50bb84d7e3174fa3d38ef4b6794593a1f20f497a
Vincent <skriosdreys@gmail.com>
2020-01-31 16:28:09 +0100

    Adding random patrol script and scripts and sprites folders

---
+++ Assets/Scripts/RandomPatrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPatrol : MonoBehaviour
+{
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+    [SerializeField] private float speed;
+
+    private Vector2 targetPosition;
+    
+    
+    void Start()
+    {
+        targetPosition = GetRandomPosition();
+    }
+    
+    void Update()
+    {
+        if ((Vector2) transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        
+        return new Vector2(randomX, randomY);
+    }
+}
