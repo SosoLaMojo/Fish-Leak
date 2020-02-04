058c6864c6faba2b40cf0a296dda135cae9c164b
William PEPIN <williamp10022000@gmail.com>
2020-02-04 12:47:42 +0100

    Adding score increased per second

---
+++ Assets/Scripts/ScoreIncreasedPerSecond.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreIncreasedPerSecond : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI scoreText;
+    float floatScoreAmount = 0;
+    int scoreAmount = 0;
+    [SerializeField] float pointIncreasePerSecond;
+
+    void Update()
+    { 
+        floatScoreAmount += pointIncreasePerSecond + Time.deltaTime;
+        scoreAmount = (int)floatScoreAmount;
+        scoreText.text = scoreAmount.ToString();
+    }
+}
