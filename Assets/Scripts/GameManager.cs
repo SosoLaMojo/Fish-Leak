bfccdaf6fcb6ac26d6458bdfeac4d23073d27d57
Solange <sol.sch1990@gmail.com>
2020-02-18 13:20:44 +0100

    Added script GameManager + Added button pause + Added panel Pause

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panelMenuPause;
+
+    public void DesactivateMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ActivateMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
