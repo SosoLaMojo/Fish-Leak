0c844872d105d67e9de9a5cf6b86d96212993e09
Gael <gael.henry.sae@gmail.com>
2020-02-19 12:56:07 +0100

    ajust name reload scene panel death and win

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,28 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void LoadScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    
+   
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
         Time.timeScale = 1;
     }
     
-    public void Restart()
-    {
-        SceneManager.LoadScene("SampleScene");
-        Time.timeScale = 1;
-    }
-
-    public void LoadLevel2()
-    {
-        SceneManager.LoadScene("SceneSolange");
-        Time.timeScale = 1;
-    }
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
